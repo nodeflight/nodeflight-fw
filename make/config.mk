f95584964706db06928e9c4a7d1e69b727f923f5
Max Sikstrom <max@pengi.se>
2020-05-02 10:44:17 +0200

    Submodules: Move to vendor directory

--- make/config.mk
+++ make/config.mk
@@ -10,32 +10,32 @@ TARGET_stm32f722_PLATFORM = stm32f7xx
 TARGET_stm32f722_CFLAGS = -DSTM32F722xx
 TARGET_stm32f722_LDFLAGS += -Tstm32f722.ld
 TARGET_stm32f722_SOURCES = \
-	platform/stm32f7xx/cmsis_device_f7/Source/Templates/gcc/startup_stm32f722xx.s
+	vendor/stm32/cmsis_device_f7/Source/Templates/gcc/startup_stm32f722xx.s
 
 TARGET_stm32f745_PLATFORM = stm32f7xx
 TARGET_stm32f745_CFLAGS = -DSTM32f745xx
 TARGET_stm32f745_LDFLAGS += -Tstm32f745.ld
 TARGET_stm32f745_SOURCES = \
-	platform/stm32f7xx/cmsis_device_f7/Source/Templates/gcc/startup_stm32f745xx.s
+	vendor/stm32/cmsis_device_f7/Source/Templates/gcc/startup_stm32f745xx.s
 
 
 #######################################################################
 # Platform configuration
 
 PLATFORM_stm32f7xx_INCLUDES = \
-	platform/stm32/cmsis_core/Core/Include \
-	platform/stm32f7xx/cmsis_device_f7/Include \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Inc \
+	vendor/stm32/cmsis_core/Core/Include \
+	vendor/stm32/cmsis_device_f7/Include \
+	vendor/stm32/stm32f7xx_hal_driver/Inc \
 	platform/stm32f7xx
 
 PLATFORM_stm32f7xx_SOURCES = \
-	platform/stm32f7xx/cmsis_device_f7/Source/Templates/system_stm32f7xx.c \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Src/stm32f7xx_hal_cortex.c \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Src/stm32f7xx_hal_dma.c \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Src/stm32f7xx_hal_gpio.c \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Src/stm32f7xx_hal_rcc.c \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Src/stm32f7xx_hal_uart.c \
-	platform/stm32f7xx/stm32f7xx_hal_driver/Src/stm32f7xx_hal.c \
+	vendor/stm32/cmsis_device_f7/Source/Templates/system_stm32f7xx.c \
+	vendor/stm32/stm32f7xx_hal_driver/Src/stm32f7xx_hal_cortex.c \
+	vendor/stm32/stm32f7xx_hal_driver/Src/stm32f7xx_hal_dma.c \
+	vendor/stm32/stm32f7xx_hal_driver/Src/stm32f7xx_hal_gpio.c \
+	vendor/stm32/stm32f7xx_hal_driver/Src/stm32f7xx_hal_rcc.c \
+	vendor/stm32/stm32f7xx_hal_driver/Src/stm32f7xx_hal_uart.c \
+	vendor/stm32/stm32f7xx_hal_driver/Src/stm32f7xx_hal.c \
 	platform/stm32f7xx/platform.c
 
 PLATFORM_stm32f7xx_CFLAGS = \
