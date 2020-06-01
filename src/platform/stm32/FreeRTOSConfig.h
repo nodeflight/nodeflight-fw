8645698f21f93ccca49b958f9885223aa1d49c56
Max Sikstrom <max@pengi.se>
2020-06-01 20:10:51 +0200

    sys: Set system task names
    let the systme task names better match the other tasks

--- src/platform/stm32/FreeRTOSConfig.h
+++ src/platform/stm32/FreeRTOSConfig.h
@@ -75,6 +75,8 @@
 #define configTIMER_TASK_PRIORITY               3
 #define configTIMER_QUEUE_LENGTH                10
 #define configTIMER_TASK_STACK_DEPTH            configMINIMAL_STACK_SIZE
+#define configTIMER_SERVICE_TASK_NAME           "sys timer"
+#define configIDLE_TASK_NAME                    "sys idle"
 
 /* Interrupt nesting behaviour configuration. */
 /* Task switching at lowest. Nothing latency critical enough for a few microseconds */
