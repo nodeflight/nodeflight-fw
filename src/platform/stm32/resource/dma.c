83a723b97e15c7cbefcb3be23778ec0359091fa8
Max Sikstrom <max@pengi.se>
2025-07-19 19:27:38 +0200

    Update copyright, remove year

--- src/platform/stm32/resource/dma.c
+++ src/platform/stm32/resource/dma.c
@@ -1,6 +1,6 @@
 /*
  * NodeFlight - platform for embedded control systems
- * Copyright (C) 2024  Max Sikström
+ * Copyright (C) Max Sikström
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
